ce4570cd64f96bcc8cf7a7c14e1ca26bbe8613a4
Stefan Ackermann <stefan.ackermann42@gmail.com>
2019-04-28 15:58:22 +0200

    simplify shuffle

--- src/model/shuffleCards.ts
+++ src/model/shuffleCards.ts
@@ -1,19 +1,8 @@
 import CardDeck from "./CardDeck";
-import {shuffle} from "lodash";
-import Ordering from "./orderings/Ordering";
-
+import {chunk, shuffle} from "lodash";
 
 export default function shuffleCards() {
     let cardsShuffled = shuffle(CardDeck);
 
-    let cardSet1 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(0, 4));
-    let cardSet2 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(4, 8));
-    let cardSet3 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(8, 12));
-    let cardSet4 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(12, 16));
-    let cardSet5 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(16, 20));
-    let cardSet6 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(20, 24));
-    let cardSet7 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(24, 28));
-    let cardSet8 = Ordering.sortByNaturalOrdering(cardsShuffled.slice(28, 32));
-
-    return [cardSet1, cardSet2, cardSet3, cardSet4, cardSet5, cardSet6, cardSet7, cardSet8];
+    return chunk(cardsShuffled, 4);
 }
